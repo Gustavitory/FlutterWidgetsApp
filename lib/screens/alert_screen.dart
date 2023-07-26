a0c8006c59fe42578641861bba3f6fc9144a2b3f
Gustavo Riera <78607300+Gustavitory@users.noreply.github.com>
2023-07-26 17:01:38 -0400

    inputs

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -4,90 +4,91 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
 
-
-  void displayDialogAndroid(BuildContext context){
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (context){
-        return AlertDialog(
-          shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10)),
-          elevation: 5,
-          title:const Text('Titulo'),
-          content: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Contenido'),
-              SizedBox(height: 10,),
-              FlutterLogo(size: 50,)
-              ]
-          ),
-          actions: [
-            TextButton(
-              onPressed: ()=>Navigator.pop(context),
-              child: const Text('Cancel')
-            ),
-            TextButton(onPressed: (){}, child: const Text('Submit'))
-          ],
-        );
-      }
-      );
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
+            title: const Text('Titulo'),
+            content: const Column(mainAxisSize: MainAxisSize.min, children: [
+              Text('Contenidos'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 50,
+              )
+            ]),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel')),
+              TextButton(onPressed: () {}, child: const Text('Submit'))
+            ],
+          );
+        });
   }
 
-  void displayDialogIos(BuildContext context){
+  void displayDialogIos(BuildContext context) {
     showCupertinoDialog(
-      barrierDismissible: false,
-      context: context, 
-      builder: (context){
-        return  CupertinoAlertDialog(
-          title:const  Text('Titulo'),
-          content:const  Column(
-            mainAxisSize: MainAxisSize.min,
-            children:[
-              Text('Este es el content de la alerta'),
-              SizedBox(height: 10,),
-              FlutterLogo(size: 50,)
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: ()=>Navigator.pop(context),
-              child: const Text('Cancel')
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Titulo'),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Este es el content de la alertaaaaa'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 50,
+                )
+              ],
             ),
-            TextButton(onPressed: ()=>Navigator.pop(context),
-             child: const Text('Ok',))
-          ],
-        );
-      }
-      );
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text(
+                    'Ok',
+                  ))
+            ],
+          );
+        });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-         child: ElevatedButton(
-          style: ElevatedButton.styleFrom(),
-          onPressed:()=> {
-            Platform.isAndroid?
-            displayDialogAndroid(context):
-            displayDialogIos(context)
-            },
-          child:const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('Mostrar alerta'),
-          )
-        ),
+      body: Center(
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(),
+            onPressed: () => {
+                  Platform.isAndroid
+                      ? displayDialogAndroid(context)
+                      : displayDialogIos(context)
+                },
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text('Mostrar alerta'),
+            )),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.close),
-        onPressed: (){
-          Navigator.pop(context);
-        }
-        ),
+          child: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
     );
   }
 }
--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,46 +1,27 @@
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class InputScreen extends StatelessWidget {
-   
-  const InputScreen({Key? key}) : super(key: key);
-  
+  const InputScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("Inputs y forms"),
+        title: const Text("Inputs y forms"),
       ),
-      body:SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10) ,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
-              TextFormField(
-                autofocus: true,
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  null;
-                },
-                //validacion:
-                validator: (value){
-                  if(value==null) return "Este campo es requerido";
-                  if(value.length<3) return "Longitud minima";
-                  return null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration:const  InputDecoration(
-                  hintText: "Nombre del usuario",
-                  labelText: "Nombre",
-                  helperText: "Solo texto",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))
-                  )
-                ),
-                
+              CustomInputField(
+                labelText: 'Nombre completo',
+                helperText: 'Solo texto',
+                icon: Icon(Icons.abc),
               ),
             ],
           ),
-          ),
+        ),
       ),
     );
   }
